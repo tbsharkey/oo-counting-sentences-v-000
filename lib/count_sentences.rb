d2ec79883dfc51f0150b3996ade9bf2ae91e74ac
Tom Sharkey <tbsharkey@gmail.com>
2019-04-24 00:01:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    x=self.split(/\.|\?|\!/)
+    x=x.delete_if {|word| word.size<2}
+    return x.size
   end
 end
